c0dff0fc5c60da2cf589969c6d0954bf1f75aed7
Stevej123 <jjanardhan@gmail.com>
2021-07-31 09:14:53 +0530

    edit dialog

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,11 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, Pipe } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, NgForm, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { ApiService } from '../api.service';
 import { getLocaleDateFormat } from '@angular/common';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { formatDate } from '@angular/common'
 
 
 
@@ -24,7 +25,8 @@ export class UserComponent implements OnInit {
     private apiService:ApiService,
     private snackbar: MatSnackBar,
     public dialogRef: MatDialogRef<any>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData | any
+    @Inject(MAT_DIALOG_DATA) public data: DialogData | any,
+    private pipe: Pipe
 
   ) { }
   onNoClick(): void {
@@ -35,7 +37,7 @@ export class UserComponent implements OnInit {
     this.userForm = this.formBuilder.group({
       name: ["",[Validators.required, Validators.minLength(4)]],
       about: ["",Validators.required],
-      dob: ["",[Validators.required]],
+      dob: [formatDate(new Date("Jan 24, 2000"), "yyyy-MM-dd", "en"),[Validators.required]],
       // Validators.pattern(/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/)
       age: ["",[Validators.required,Validators.pattern('[0-9]*')]],
       phone: ["",[Validators.required,Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
@@ -50,15 +52,55 @@ export class UserComponent implements OnInit {
       let id = this.data.dialogText;
       this.apiService.getDataById(id)          
           .subscribe(x =>{
-            console.log("id value", x);
+            console.log("id value",  this.data.dialagText);
+            let idValue = this.data.dialagText;
             let value = x;
-            let obj = value.findIndex(o => o._id === id);
+            let obj = value.find(o => o._id === idValue);
             console.log("obj", obj);
             // this.userForm.patchValue(x)
+            let dateConversion = new Date(obj.dob);
+            // let monthFormat = dateConversion.getMonth() < 10 ? ('0'+ dateConversion.getMonth() ) : dateConversion.getMonth();
+            let formatted_date = dateConversion.getDate() + "-" + (dateConversion.getMonth() + 1) + "-" + dateConversion.getFullYear()
+
+            let formatedDate = formatDate(obj.dob,'dd-MM-yyyy','en');
+
+
+            console.log("formated date", formatDate(obj.dob,'dd-MM-yyyy','en'));
+            // this.userForm.get('dob').setValue({
+            //   year: parseInt(formatedDate.format('YYYY'), 10),
+            //   month: parseInt(formatedDate.format('M'), 10),
+            //   day: parseInt(formatedDate.format('D'), 10)
+            // });
+             
+            this.userForm.patchValue({
+              name: obj.name,
+              about: obj.about,
+              dob:formatDate(new Date(obj.dob), "yyyy-MM-dd", "en"),
+              // dob:this.pipe.transform(
+              //   this.addEmployeeForm.value.DOB,
+              //   "yyyy-MM-dd"
+              // ),
+              age: obj.age,
+              phone: obj.phone,
+              email: obj.email,
+              linkedin_profile: obj.linkedin_profile
+              
+              }
+            );
           }) ;
   }
 
    
+  }
+
+  formatDate(date:any) {
+    const d = new Date(date);
+    let month = '' + (d.getMonth() + 1);
+    let day = '' + d.getDate();
+    const year = d.getFullYear();
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+    return [day, month,  year].join('-');
   }
 
   getUserDetail(event:any){
